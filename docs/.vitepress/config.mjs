87098ac42fd1a36fbf4901c0aeeeaf9cd285cf9e
Wolfe <165944055+WolfeOvO@users.noreply.github.com>
2026-01-11 20:19:21 +0800

    Update navigation links in config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,8 +10,8 @@ export default defineConfig({
     // 1. 顶部导航栏
     nav: [
       { text: '首页', link: '/' },
-      { text: '储物间', link: '/storage/home' },
-      { text: '墙外指南', link: '/gfw-guide/basic' }
+      { text: '储物间', link: '/storage/contents.md' },
+      { text: '墙外指南', link: '/gfw-guide/contents.md' }
     ],
 
     // 2. 侧边栏 (多侧边栏配置 - 对象格式)
@@ -20,19 +20,19 @@ export default defineConfig({
         {
           text: '储物间',
           items: [
-            { text: '索引', link: '/storage/home' },
-            { text: '搜索引擎', link: '/storage/search' },
-            { text: '问答导航', link: '/storage/q&a' },
-            { text: '百科全书', link: '/storage/encyclopedia' },
-            { text: 'BT 导航', link: '/storage/bt' },
-            { text: '网盘服务', link: '/storage/cloud' },
-            { text: '音乐导航', link: '/storage/music' },
-            { text: '社交平台', link: '/storage/social' },
-            { text: '图书馆', link: '/storage/library' },
-            { text: '图片资源', link: '/storage/photo' },
-            { text: '网页浏览器', link: '/storage/browser' },
-            { text: '维基媒体', link: '/storage/wikimedia' },
-            { text: '工具箱', link: '/storage/toolbox' },
+            { text: '目录', link: '/storage/contents.md' },
+            { text: '搜索引擎', link: '/storage/search.md' },
+            { text: '问答导航', link: '/storage/q&a.md' },
+            { text: '百科全书', link: '/storage/encyclopedia.md' },
+            { text: 'BT 导航', link: '/storage/bt.md' },
+            { text: '网盘服务', link: '/storage/cloud.md' },
+            { text: '音乐导航', link: '/storage/music.md' },
+            { text: '社交平台', link: '/storage/social.md' },
+            { text: '图书馆', link: '/storage/library.md' },
+            { text: '图片资源', link: '/storage/photo.md' },
+            { text: '网页浏览器', link: '/storage/browser.md' },
+            { text: '维基媒体', link: '/storage/wikimedia.md' },
+            { text: '工具箱', link: '/storage/toolbox.md' },
           ]
         }
       ],
@@ -40,6 +40,7 @@ export default defineConfig({
         {
           text: '墙外指南',
           items: [
+            { text: '目录', link: '/gfw-guide/contents.md' },
             { text: '基础篇', link: '/gfw-guide/basic' },
             { text: '进阶篇', link: '/gfw-guide/advanced' }
           ]
