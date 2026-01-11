2c1af88e86515207383a94d351eedef32fa14076
Wolfe <165944055+WolfeOvO@users.noreply.github.com>
2026-01-11 20:00:14 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,7 +10,19 @@ export default defineConfig({
     nav: [
       { text: '首页', link: '/' },
       // 注意：这里的 link 最好指向该分类下的第一篇文章
-      { text: '储物间', link: '/storage/storage' },
+      { text: '储物间', link: '/storage/home' },
+      { text: '搜索引擎'，search.md)
+2. [问答导航](q&a.md)
+3. [百科全书](encyclopedia.md)
+4. [BT 导航](bt.md)
+5. [网盘服务](cloud.md)
+6. [音乐导航](music.md)
+7. [社交平台](social.md)
+8. [图书馆](library.md)
+9. [图片资源](photo.md)
+10. [网页浏览器](broswer.md)
+11. [维基媒体](wikimedia.md)
+12. [工具箱](toolbox.md)
       { text: '墙外指南', link: '/gfw-guide/basic' }
     ],
 
@@ -18,7 +30,7 @@ export default defineConfig({
     sidebar: {
       '/storage/': [
         {
-          text: '储物间',
+          text: '储物间'，
           items: [
             { text: '索引', link: '/storage/home' },
             { text: '1 搜索引擎', link: '/storage/1' },
