365e22aafdd82b1a09a7c360facc6b873402fe5e
WolfeOvO <WolfeOvO@users.noreply.github.com>
2026-01-16 13:32:23 +0000

    deploy: 6cb4c7263342e2451644b4fd2d5ba2946e521d3b

---
+++ assets/墙外指南_客户端下载_各大客户端.md.CsvCgOrp.lean.js
@@ -0,0 +1,7 @@
+import{_ as s,C as n,c as i,o as d,ai as h,G as t,j as e,w as r}from"./chunks/framework.DTiVPgWN.js";const g=JSON.parse('{"title":"各大客户端","description":"","frontmatter":{},"headers":[],"relativePath":"墙外指南/客户端下载/各大客户端.md","filePath":"墙外指南/客户端下载/各大客户端.md","lastUpdated":1768570315000}'),u={name:"墙外指南/客户端下载/各大客户端.md"};function b(p,a,f,_,m,C){const o=n("PluginTabs"),l=n("PluginTabsTab");return d(),i("div",null,[a[3]||(a[3]=h("",8)),t(o,null,{default:r(()=>[t(l,{label:"第一层：系统"},{default:r(()=>[a[0]||(a[0]=e("p",null,"这是第一层的内容。",-1)),t(o)]),_:1}),t(l,{label:"第二层：安装方式"},{default:r(()=>[...a[1]||(a[1]=[e("pre",null,[e("code",null,`这里是嵌套在里面的第二层。
+
+| 表格 | 支持 |
+| :--- | :--- |
+| 测试 | ✅ |
+`)],-1)])]),_:1}),t(l,{label:"第二层：手动安装"},{default:r(()=>[...a[2]||(a[2]=[e("pre",null,[e("code",null,`这里是手动安装说明。
+`)],-1)])]),_:1})]),_:1}),a[4]||(a[4]=e("p",null,"== 第一层：关于",-1)),a[5]||(a[5]=e("p",null,"这是关于的内容。",-1)),a[6]||(a[6]=e("p",null,":::",-1))])}const k=s(u,[["render",b]]);export{g as __pageData,k as default};
