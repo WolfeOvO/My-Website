1905b983bb5795f0cb215942c4a59515f62c676d
Wolfe <165944055+WolfeOvO@users.noreply.github.com>
2026-01-17 00:41:04 +0800

    添加向导

---
+++ docs/.vitepress/components/vitepress-tabs/index.js
@@ -0,0 +1,15 @@
+import Tabs from './Tabs.vue'
+import Tab from './Tab.vue'
+import IconTabs from './IconTabs.vue'
+import IconTab from './IconTab.vue'
+import Alert from './Alert.vue'
+import Button from './Button.vue'
+
+export {
+  Tabs,
+  Tab,
+  IconTabs,
+  IconTab,
+  Alert,
+  Button
+}
