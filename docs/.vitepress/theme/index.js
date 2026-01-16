45406c3b8cc192ef5e40030e814a98b92f62bf4f
Wolfe <165944055+WolfeOvO@users.noreply.github.com>
2026-01-16 22:44:55 +0800

    更新组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import './style.css'
 import DefaultTheme from 'vitepress/theme'
-import SidebarList from '../components/SidebarList.vue'
 import Spoiler from '../components/Spoiler.vue'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import GitHubRelease from '../components/GitHubRelease.vue'
@@ -9,7 +8,6 @@ import SubSidebar from '../components/SubSidebar.vue'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    app.component('SidebarList', SidebarList),
     app.component('Spoiler', Spoiler),
     enhanceAppWithTabs(app),
     app.component('GitHubRelease', GitHubRelease),
